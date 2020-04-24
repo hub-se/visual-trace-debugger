cdf106d2d7a2f5ec7c933bcc35f22554a6040a5d
heidensi <heiden@informatik.hu-berlin.de>
2020-04-24 23:27:10 +0200

    initial sample plugin project setup

---
+++ src/main/java/icons/VisualTraceDebuggerIcons.java
@@ -0,0 +1,11 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface VisualTraceDebuggerIcons {
+
+    Icon VISUAL_TRACE = IconLoader.getIcon("/icons/visualTrace.svg");
+
+}
